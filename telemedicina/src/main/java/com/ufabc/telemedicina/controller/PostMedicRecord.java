cd6d3c8beb72536f41d458f2ab3bcc2b378bd834
sergioppires <sergioppires@live.com>
2019-11-21 22:01:05 -0300

    Implementação do post

--- telemedicina/src/main/java/com/ufabc/telemedicina/controller/PostMedicRecord.java
+++ telemedicina/src/main/java/com/ufabc/telemedicina/controller/PostMedicRecord.java
@@ -1,4 +1,27 @@
 package com.ufabc.telemedicina.controller;
 
+import com.ufabc.telemedicina.domains.Paciente;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/projetoaed")
 public class PostMedicRecord {
+
+    @PostMapping(value="/paciente/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Paciente> requestPaciente(
+            @PathVariable(value="id") String documento,
+            @RequestBody Paciente payload) {
+        Paciente paciente = new Paciente();
+        paciente.setCpf(documento);
+        paciente.setIdade("12");
+        HttpHeaders responseHeaders = new HttpHeaders();
+        responseHeaders.set("Chave", "AED2");
+        return new ResponseEntity<Paciente>(paciente, responseHeaders, HttpStatus.OK);
+    }
+
 }
